665f7ee005dee1dd41a662b8f16e1200eb50c189
sandu345 <sandunibatugedara11@gmail.com>
2024-12-23 19:49:05 +0530

    first

--- tests/MyFirst_Test.spec.js
+++ tests/MyFirst_Test.spec.js
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const name = await page.innerText('.navbar__title');
-  expect(name).toBe('Playwright');
+test("My First Test", async ({ page }) => {
+  await page.goto("https://google.com/");
+  // await page.waitForLoadState('domcontentloaded');
+  await expect(page).toHaveTitle("Google");
 });
