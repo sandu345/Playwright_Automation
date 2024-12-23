4d23da44dee5fc9e78b54ab20b585e3d3f51f560
sandu345 <sandunibatugedara11@gmail.com>
2024-12-23 22:27:53 +0530

    first

--- tests/MyFirst_Test.spec.js
+++ tests/MyFirst_Test.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("My First Test", async ({ page }) => {
-  await page.goto("https://google.com/");
+  await page.goto("https://practicesoftwaretesting.com/");
   // await page.waitForLoadState('domcontentloaded');
-  await expect(page).toHaveTitle("Google");
+  await expect(page).toHaveTitle("Toolshop");
 });
